bea5ea0d572aafe521edd631c6cdac812388bda7
pradeepyadav5137 <pradeepapex27@gmail.com>
2025-11-04 21:39:18 +0530

    upadate

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/pradeep/Desktop/ml project/venv'
+set -gx VIRTUAL_ENV /home/pradeep/Desktop/Pradeep_205124066_B/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
